b0e24bd46f70a91626a30cc2b06489ef315a4af3
Nikolay <nkrasovskii@sfedu.ru>
2024-12-21 00:59:03 +0300

    commit

---
+++ PETask/PETask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Великая теорема Ферма: нет решений для a^n + b^n = c^n, n > 2.");
+        int a = 3, b = 4, c = 5, n = 3;
+        bool isTrue = Math.Pow(a, n) + Math.Pow(b, n) != Math.Pow(c, n);
+        Console.WriteLine($"Проверка для a={a}, b={b}, c={c}, n={n}: {isTrue}");
+    }
+}
