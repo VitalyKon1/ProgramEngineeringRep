36e78d9fcb2afc25a82885f10f36c01f41d7ab4e
Nikolay <nkrasovskii@sfedu.ru>
2024-12-21 01:20:08 +0300

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,6 @@ class Program
     {
         Console.WriteLine("Великая теорема Ферма: нет решений для a^n + b^n = c^n, n > 2.");
         int a = 3, b = 4, c = 5, n = 3;
-        bool isTrue = Math.Pow(a, n) + Math.Pow(b, n) != Math.Pow(c, n);
-        Console.WriteLine($"Проверка для a={a}, b={b}, c={c}, n={n}: {isTrue}");
+        Console.WriteLine($"Проверка для a={a}, b={b}, c={c}, n={n}: {Math.Pow(a, n) + Math.Pow(b, n) != Math.Pow(c, n)}");
     }
 }
